ffbbf00f20ee78f9432456319dc42f810eaba007
Varun Jain <varunj4700@gmail.com>
2022-03-19 15:20:58 +0530

    Time: 245 ms (68.17%), Space: 79.5 MB (88.47%) - LeetHub

---
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -0,0 +1,34 @@
+class FreqStack
+{
+    public:
+        stack<int> st;
+    priority_queue<pair<pair<int, int>, int>> pq;
+    unordered_map<int, int> freq;
+    int t = 0;
+    FreqStack() {}
+
+    void push(int val)
+    {
+        t++;
+        freq[val]++;
+        st.push(val);
+        pq.push({{ freq[val], t},val });
+    }
+
+    int pop()
+    {
+        t++;
+        pair<pair<int, int>, int> p = pq.top();
+        pq.pop();
+        int num = p.second;
+        freq[num]--;
+        return num;
+    }
+};
+
+/**
+ *Your FreqStack object will be instantiated and called as such:
+ *FreqStack* obj = new FreqStack();
+ *obj->push(val);
+ *int param_2 = obj->pop();
+ */
