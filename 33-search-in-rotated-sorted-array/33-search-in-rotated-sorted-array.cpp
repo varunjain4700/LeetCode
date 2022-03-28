156e1e2e58a14cecb6a33e433f38bbc55b84346e
Varun Jain <varunj4700@gmail.com>
2022-03-28 20:19:05 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (29.80%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,51 +1,30 @@
 class Solution
 {
     public:
-        int bs(vector<int> &nums, int low, int high, int target)
+        int search(vector<int> &nums, int target)
         {
-            int start = low, end = high, mid;
-            while (start <= end)
+            int low = 0, high = nums.size() - 1, mid;
+            while (low <= high)
             {
-               	//cout<<start<<" "<<end<<endl;
-                mid = (start + end) / 2;
+                mid = (low + high) / 2;
+               	//cout<<low<<" "<<high<<endl;
                 if (target == nums[mid])
                     return mid;
-                else if (target < nums[mid])
-                    end = mid - 1;
+                if (nums[low] <= nums[mid])
+                {
+                    if (target >= nums[low] && target <= nums[mid])
+                        high = mid - 1;
+                    else
+                        low = mid + 1;
+                }
                 else
-                    start = mid + 1;
+                {
+                    if (target >= nums[mid] && target <= nums[high])
+                        low = mid + 1;
+                    else
+                        high = mid - 1;
+                }
             }
             return -1;
         }
-    int search(vector<int> &nums, int target)
-    {
-        int n = nums.size();
-        int low = 0, high = n - 1, mid;
-        int minm = 0;
-        while (low <= high)
-        {
-            mid = (low + high) / 2;
-           	//cout<<low<<" "<<high<<endl;
-            if (nums[low] <= nums[high])
-            {
-                minm = low;
-                break;
-            }
-            int prev = (mid - 1 + n) % n, next = (mid + 1) % n;
-            if (nums[mid] <= nums[prev] && nums[mid] <= nums[next])
-            {
-                minm = mid;
-                break;
-            }
-            if (nums[low] <= nums[mid])
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-       	// cout<<minm<<endl;
-        int idx = bs(nums, 0, minm - 1, target);
-        if (idx == -1)
-            idx = bs(nums, minm, n - 1, target);
-        return idx;
-    }
 };
