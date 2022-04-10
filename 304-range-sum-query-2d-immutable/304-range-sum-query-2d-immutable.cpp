783bbcf4602ae6b8d145cb1ead7d4bf066fd3c96
Varun Jain <varunj4700@gmail.com>
2022-04-10 17:54:10 +0530

    Time: 364 ms (98.16%), Space: 148 MB (50.13%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,30 @@
+class NumMatrix
+{
+    public:
+       	//If asked to answer each query in O(n),then it can be done easily using prefix sums(traversing all the rows one by one)
+       	// To do it in O(1),need to use a different approach-To understand it see the last approach in solution section
+        vector<vector < int>> dp;
+    NumMatrix(vector<vector < int>> &mat)
+    {
+        int n = mat.size(), m = mat[0].size();
+        dp.resize(n + 1, vector<int> (m + 1, 0));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                dp[i + 1][j + 1] = mat[i][j] + dp[i + 1][j] + dp[i][j + 1] - dp[i][j];
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2)
+    {
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+    }
+};
+
+/**
+ *Your NumMatrix object will be instantiated and called as such:
+ *NumMatrix* obj = new NumMatrix(matrix);
+ *int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
