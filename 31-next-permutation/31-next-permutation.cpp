d677cc290cb7b8cfaf0fffc303c6cd0c05df1c50
Varun Jain <varunj4700@gmail.com>
2022-05-17 02:26:29 +0530

    Time: 7 ms (51.62%), Space: 12 MB (93.41%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -4,25 +4,53 @@ class Solution
         void nextPermutation(vector<int> &nums)
         {
             int n = nums.size();
-            int i, idx2, idx1 = -1;
-            for (int i = n - 1; i >= 0; i--)
+            int idx1 = -1, idx2;
+            for (int i = n - 1; i >= 1; i--)
             {
-                for (int j = i - 1; j >= 0; j--)
+                if (nums[i - 1] < nums[i])
                 {
-                    if (nums[j] < nums[i] && j > idx1)
-                    {
-                        idx1 = max(idx1, j);
-                        idx2 = i;
-                        break;
-                    }
+                    idx1 = i - 1;
+                    break;
                 }
             }
             if (idx1 == -1)
             {
-                sort(nums.begin(), nums.end());
+                reverse(nums.begin(), nums.end());
                 return;
             }
+            for (int i = n - 1; i >= 0;i--)
+            {
+                if (nums[i] > nums[idx1])
+                {
+                    idx2 = i;
+                    break;
+                }
+            }
             swap(nums[idx1], nums[idx2]);
-            sort(nums.begin() + idx1 + 1, nums.end());
+            reverse(nums.begin() + idx1+1, nums.end());
+
+           	//O(n^2) approach
+
+           	// int n = nums.size();
+           	// int i, idx2, idx1 = -1;
+           	// for (int i = n - 1; i >= 0; i--)
+           	// {
+           	//     for (int j = i - 1; j >= 0; j--)
+           	//     {
+           	//         if (nums[j] < nums[i] && j > idx1)
+           	//         {
+           	//             idx1 = max(idx1, j);
+           	//             idx2 = i;
+           	//             break;
+           	//         }
+           	//     }
+           	// }
+           	// if (idx1 == -1)
+           	// {
+           	//     sort(nums.begin(), nums.end());
+           	//     return;
+           	// }
+           	// swap(nums[idx1], nums[idx2]);
+           	// sort(nums.begin() + idx1 + 1, nums.end());
         }
 };
