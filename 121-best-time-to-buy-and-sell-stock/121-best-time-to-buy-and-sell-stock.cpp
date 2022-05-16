ae2e482a651b8df84ba2facb2c5b36baa714e6da
Varun Jain <varunj4700@gmail.com>
2022-05-17 02:54:17 +0530

    Time: 203 ms (24.12%), Space: 93.4 MB (53.77%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,27 +1,34 @@
 class Solution
 {
     public:
-        int solve(int idx, int buy, vector<int> &prices, vector<vector< int>> &dp)
+       	// int solve(int idx, int buy, vector<int> &prices, vector<vector < int>> &dp)
+       	// {
+       	//     if (idx == prices.size())
+       	//         return 0;
+       	//     if (dp[idx][buy] != -1)
+       	//         return dp[idx][buy];
+       	//     int ans = 0;
+       	//     if (buy == 1)
+       	//     {
+       	//         ans = max(-prices[idx] + solve(idx + 1, 0, prices, dp), solve(idx + 1, 1, prices, dp));
+       	//     }
+       	//     else if (buy == 0)
+       	//     {
+       	//         ans = max(prices[idx], solve(idx + 1, 0, prices, dp));
+       	//     }
+       	//     return dp[idx][buy] = ans;
+       	// }
+        int maxProfit(vector<int> &prices)
         {
-            if (idx == prices.size())
-                return 0;
-            if (dp[idx][buy] != -1)
-                return dp[idx][buy];
-            int ans = 0;
-            if (buy == 1)
+            int n = prices.size();
+           	// vector<vector < int>> dp(n + 1, vector<int> (2, -1));
+           	// return solve(0, 1, prices, dp);
+            int minm = prices[0], ans = 0;
+            for (int i = 0; i < n; i++)
             {
-                ans = max(-prices[idx] + solve(idx + 1, 0, prices, dp), solve(idx + 1, 1, prices, dp));
+                ans = max(ans, prices[i] - minm);
+                minm = min(prices[i], minm);
             }
-            else if (buy == 0)
-            {
-                ans = max(prices[idx], solve(idx + 1, 0, prices, dp));
-            }
-            return dp[idx][buy] = ans;
+            return ans;
         }
-    int maxProfit(vector<int> &prices)
-    {
-        int n = prices.size();
-        vector<vector < int>> dp(n + 1, vector<int> (2, -1));
-        return solve(0, 1, prices, dp);
-    }
 };
