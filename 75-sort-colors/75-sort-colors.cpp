a3f572d6ac2ae89691b7bc6e0bb7595406420453
Varun Jain <varunj4700@gmail.com>
2022-05-17 03:12:06 +0530

    Time: 4 ms (41.73%), Space: 8.2 MB (71.20%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,28 +1,26 @@
 class Solution
 {
     public:
-       	//first approach is to sort the array by any sorting algorithm- time complexity will be O(nlogn).
-       	//second approach is by counting the instances of 0's,1's and 2's - time complexity be O(n) but will need 2 passes atleast.
-       	//Hence, the most optimal approach is by using just one pass.
         void sortColors(vector<int> &nums)
         {
             int n = nums.size();
-            int start = 0, end = n - 1;
-            int mid = 0;
-            while (mid <= end)
+            int i = 0, j = n - 1, k = 0;
+            while (i <= j)
             {
-                if (nums[mid] == 0)
+                if (nums[i] == 0)
                 {
-                    swap(nums[mid], nums[start]);
-                    mid++;
-                    start++;
+                    swap(nums[i], nums[k]);
+                    i++;
+                    k++;
                 }
-                else if (nums[mid] == 1)
-                    mid++;
+                else if (nums[i] == 1)
+                {
+                    i++;
+                }
                 else
                 {
-                    swap(nums[mid], nums[end]);	//mid pointer will not move forward
-                    end--;
+                    swap(nums[i], nums[j]);
+                    j--;
                 }
             }
         }
