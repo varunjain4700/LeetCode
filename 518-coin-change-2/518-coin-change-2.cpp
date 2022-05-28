4f6d07365a26911d0013556625b66468624caa20
Varun Jain <varunj4700@gmail.com>
2022-05-28 12:27:31 +0530

    Time: 97 ms (12.72%), Space: 19.1 MB (15.13%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -20,29 +20,28 @@ class Solution
                 ans = solve(idx + 1, amount, coins, n, dp);
             return dp[idx][amount] = ans;
         }
-    int iterative(vector<int> &coins, int amount, int n)
-    {
-        vector<vector < int>> dp(n+1, vector<int> (amount + 1, 0));
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 1;
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= amount; j++)
-            {
-                if (coins[i - 1] <= j)
-                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
-                else
-                    dp[i][j] = dp[i - 1][j];
-            }
-        }
-        return dp[n][amount];
-    }
+    // int iterative(vector<int> &coins, int amount, int n)
+    // {
+    //     vector<vector < int>> dp(n+1, vector<int> (amount + 1, 0));
+    //     for (int i = 0; i <= n; i++)
+    //         dp[i][0] = 1;
+    //     for (int i = 1; i <= n; i++)
+    //     {
+    //         for (int j = 1; j <= amount; j++)
+    //         {
+    //             if (coins[i - 1] <= j)
+    //                 dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+    //             else
+    //                 dp[i][j] = dp[i - 1][j];
+    //         }
+    //     }
+    //     return dp[n][amount];
+    // }
     int change(int amount, vector<int> &coins)
     {
-        int res = 0;
         int n = coins.size();
         vector<vector < int>> dp(n + 1, vector<int> (amount + 1, -1));
-       	//return solve(0, amount, coins, n, dp);
-        return iterative(coins, amount, n);
+       	return solve(0, amount, coins, n, dp);
+        //return iterative(coins, amount, n);
     }
 };
