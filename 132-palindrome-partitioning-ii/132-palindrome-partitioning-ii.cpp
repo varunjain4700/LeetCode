f282636be3b75c9bc40fac820ab0e4291a38debd
Varun Jain <varunj4700@gmail.com>
2022-05-30 16:13:58 +0530

    Time: 1119 ms (5.04%), Space: 385.2 MB (5.20%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+    public:
+        bool check(string& str)
+        {
+            int n = str.size();
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (str[i] != str[n - i - 1])
+                    return false;
+            }
+            return true;
+        }
+    int solve(int idx, string s, int n, vector<int> &dp)
+    {
+        if (idx == n)
+            return 0;
+        if (dp[idx] != -1)
+            return dp[idx];
+        int ans = 1e5;
+        string str = "";
+        for (int i = idx; i < n; i++)
+        {
+            str += s[i];
+            if (check(str))
+            {
+                ans = min(ans, 1 + solve(i + 1, s, n, dp));
+            }
+        }
+        return dp[idx] = ans;
+    }
+    int minCut(string s)
+    {
+        int n = s.size();
+        vector<int> dp(n + 1, -1);
+        return solve(0, s, n, dp) - 1;
+    }
+};
