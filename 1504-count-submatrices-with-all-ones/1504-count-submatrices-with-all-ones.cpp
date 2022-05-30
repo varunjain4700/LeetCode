07613b3999a59c5bd207e831af3a663cdbf8ca83
Varun Jain <varunj4700@gmail.com>
2022-05-30 15:12:42 +0530

    Time: 234 ms (20.67%), Space: 15.1 MB (47.73%) - LeetHub

---
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    public:
+        int solve(vector<int> &nums, int val)
+        {
+            int n = nums.size();
+            int cnt = 0, i = 0, j = 0;
+            while (i < n)
+            {
+                if (nums[i] != val)
+                {
+                    j = i + 1;
+                }
+                cnt += i - j + 1;
+                i++;
+            }
+            return cnt;
+        }
+    int numSubmat(vector<vector < int>> &mat)
+    {
+        int m = mat.size(), n = mat[0].size();
+        int ans = 0;
+        for (int i = 0; i < m; i++)
+        {
+            vector<int> temp(n, 0);
+            int val = 0;
+            for (int j = i; j < m; j++)
+            {
+                val++;
+                for (int k = 0; k < n; k++)
+                {
+                    temp[k] += mat[j][k];
+                }
+                ans += solve(temp, val);
+            }
+        }
+        return ans;
+    }
+};
