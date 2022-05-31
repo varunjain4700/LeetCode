92ea79fc3e8c38a74bacfac391c1f1d68731ed8c
Varun Jain <varunj4700@gmail.com>
2022-05-31 11:30:36 +0530

    Time: 1634 ms (31.89%), Space: 14.4 MB (76.46%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,7 +1,9 @@
 class Solution
 {
     public:
+    //very strange that if we take array instead of vector it doesn't give TLE
     int dp[70][70*70+1];
+    // vector<vector < int>> dp;
         int solve(int idx, int sum, vector<vector < int>> &mat, int target)
         {
             if (idx == mat.size())
@@ -20,7 +22,8 @@ class Solution
     int minimizeTheDifference(vector<vector < int>> &mat, int target)
     {
         int m = mat.size(), n = mat[0].size(), sum = 0;
-        memset(dp,-1,sizeof(dp));
+        // dp.resize(70,vector<int>(4901,-1));
+         memset(dp,-1,sizeof(dp));
         //vector<vector < int>> dp(m + 1, vector<int> ((70 * 70) + 1, -1));
         return solve(0, 0, mat, target);
     }
