23844c2782e1193be5fac55f5f89b37c4f67cf25
Varun Jain <varunj4700@gmail.com>
2022-06-03 21:50:06 +0530

    Time: 746 ms (23.98%), Space: 148 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,25 +1,28 @@
 class NumMatrix
 {
     public:
-       	//If asked to answer each query in O(n),then it can be done easily using prefix sums(traversing all the rows one by one)
-       	// To do it in O(1),need to use a different approach-To understand it see the last approach in solution section
         vector<vector < int>> dp;
     NumMatrix(vector<vector < int>> &mat)
     {
-        int n = mat.size(), m = mat[0].size();
-        dp.resize(n + 1, vector<int> (m + 1, 0));
-        for (int i = 1; i <= n; i++)
+        int m = mat.size(), n = mat[0].size();
+        dp.resize(m + 1, vector<int> (n + 1, 0));
+        for (int i = 0; i < m; i++)
         {
-            for (int j = 1; j <= m; j++)
+            for (int j = 0; j < n; j++)
             {
-                dp[i][j] = mat[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+                dp[i + 1][j + 1] += mat[i][j];
+                dp[i + 1][j + 1] += dp[i][j + 1];
+                dp[i + 1][j + 1] += dp[i + 1][j];
+                dp[i + 1][j + 1] -= dp[i][j];
+               	//cout << dp[i+1][j+1] << " ";
             }
+           	// cout << endl;
         }
     }
 
     int sumRegion(int row1, int col1, int row2, int col2)
     {
-        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1];
+        return dp[row2 + 1][col2 + 1] - dp[row2 + 1][col1] - dp[row1][col2 + 1] + dp[row1][col1];
     }
 };
 
