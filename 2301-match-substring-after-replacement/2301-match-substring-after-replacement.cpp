b7172c1139db88ac995e67f4d41e2c0cc8e6073d
Varun Jain <varunj4700@gmail.com>
2022-06-16 20:23:54 +0530

    Time: 1949 ms (5.12%), Space: 18.1 MB (64.79%) - LeetHub

---
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+    public:
+        bool matchReplacement(string s, string sub, vector<vector < char>> &mappings)
+        {
+            int n = s.size(), m = sub.size();
+            unordered_map<int, set < int>> mp;
+            for (int i = 0; i < mappings.size(); i++)
+            {
+                mp[mappings[i][0]].insert(mappings[i][1]);
+            }
+            int j = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i - j + 1 == m)
+                {
+                    int k, l = i - m + 1;
+                    for (k = 0; k < m; k++)
+                    {
+                        if (sub[k] == s[l])
+                        {
+                            l++;
+                            continue;
+                        }
+                        else
+                        {
+                            if (mp[sub[k]].find(s[l]) == mp[sub[k]].end())
+                                break;
+                        }
+                        l++;
+                    }
+                    if (k == m)
+                        return true;
+                    j++;
+                }
+            }
+            return false;
+        }
+};
