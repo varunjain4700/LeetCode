74fa7835e3686b83596a520a59a124755cbd56a7
Varun Jain <varunj4700@gmail.com>
2022-06-17 21:58:17 +0530

    Time: 14 ms (68.31%), Space: 21.3 MB (55.06%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -13,16 +13,36 @@ class Solution
 {
     public:
        	//Main observations:don't install cameras on leaf nodes and start putting cameras from bottom to top.
-        unordered_set<TreeNode*> covered;	//keeping a track of nodes that will be covered by the cameras
-    int cameras = 0;
+       	//  unordered_set<TreeNode*> covered;	//keeping a track of nodes that will be covered by the cameras
+        int cameras = 0;
     int minCameraCover(TreeNode *root)
     {
         cameras = 0;
-        covered.insert(NULL);	//for leaf nodes(as we don't want to install a camera on leaf node)
-        solve(root, NULL);
+       	// covered.insert(NULL);	//for leaf nodes(as we don't want to install a camera on leaf node)
+       	//solve(root, NULL);
+        if (solve2(root) == 0)
+            cameras++;
         return cameras;
     }
-    void solve(TreeNode *root, TreeNode *par)
+   	//0-->node is not covered by the camera
+   	//1-->covered by the camera
+   	//2-->has the camera itself
+    int solve2(TreeNode *root)
+    {
+        if (!root)
+            return 1;
+        int left = solve2(root->left);
+        int right = solve2(root->right);
+        if (left == 0 || right == 0)
+        {
+            cameras++;
+            return 2;
+        }
+        else if (left == 2 || right == 2)
+            return 1;
+        else return 0;
+    }
+    /*void solve(TreeNode *root, TreeNode *par)
     {
         if(!root)
             return;
@@ -40,5 +60,5 @@ class Solution
             covered.insert(root->left);
             covered.insert(root->right);
         }
-    }
+    }*/
 };
