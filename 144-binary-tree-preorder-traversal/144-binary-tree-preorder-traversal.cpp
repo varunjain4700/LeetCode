302aa673a5f3d97f718b7256cc89f9020b70eb3c
Varun Jain <varunj4700@gmail.com>
2022-06-17 13:54:58 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (99.51%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -40,8 +40,8 @@ class Solution
     vector<int> preorderTraversal(TreeNode *root)
     {
         vector<int> preorder;
-       	//recursive(root, preorder);
-        iterative(root, preorder);
+       	recursive(root, preorder);
+        //iterative(root, preorder);
         return preorder;
     }
 };
