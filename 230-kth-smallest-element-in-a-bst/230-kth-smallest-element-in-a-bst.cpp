a053baaca759260b142b818bebb55f3bb929f573
Varun Jain <varunj4700@gmail.com>
2022-06-27 18:23:18 +0530

    Time: 43 ms (9.42%), Space: 24.2 MB (59.11%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -1,31 +1,34 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        int ans;
-    void solve(TreeNode *root, int &k)
-    {
-        if (!root)
-            return;
-        solve(root->left, k);
-        k--;
-        if (k == 0)
-            ans = root->val;
-        solve(root->right, k);
-    }
+        void solve(TreeNode *root, int &k, int &ans)
+        {
+            if (!root)
+                return;
+            solve(root->left, k, ans);
+            k--;
+            if (k == 0)
+            {
+                ans = root->val;
+                return;
+            }
+            solve(root->right, k, ans);
+        }
     int kthSmallest(TreeNode *root, int k)
     {
-        solve(root, k);
+        int ans = 0;
+        solve(root, k, ans);
         return ans;
     }
 };
