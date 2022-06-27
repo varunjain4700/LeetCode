b8a744eadbad21e7c55a5dc2624150bde0159075
Varun Jain <varunj4700@gmail.com>
2022-06-27 16:03:07 +0530

    Time: 142 ms (39.99%), Space: 56.9 MB (48.73%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -1,48 +1,73 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        void insert(TreeNode *root, int val)
+       	// void insert(TreeNode *root, int val)
+       	// {
+       	//     if (!root)
+       	//         return;
+       	//     if (root->val > val)
+       	//     {
+       	//         if (!root->left)
+       	//         {
+       	//             root->left = new TreeNode(val);
+       	//             return;
+       	//         }
+       	//         insert(root->left, val);
+       	//     }
+       	//     else
+       	//     {
+       	//         if (!root->right)
+       	//         {
+       	//             root->right = new TreeNode(val);
+       	//             return;
+       	//         }
+       	//         insert(root->right, val);
+       	//     }
+       	// }
+        TreeNode* insertIntoBST(TreeNode *root, int val)
         {
             if (!root)
-                return;
-            if (root->val > val)
             {
-                if (!root->left)
-                {
-                    root->left = new TreeNode(val);
-                    return;
-                }
-                insert(root->left, val);
+                root = new TreeNode(val);
+                return root;
             }
-            else
+           	// insert(root, val);
+           	// return root;
+
+           	//ITERATIVE APPROACH
+            TreeNode *temp = root;
+            while (root != NULL)
             {
-                if (!root->right)
+                if (root->val > val)
                 {
-                    root->right = new TreeNode(val);
-                    return;
+                    if (!root->left)
+                    {
+                        root->left = new TreeNode(val);
+                        return temp;
+                    }
+                    root = root->left;
                 }
-                insert(root->right, val);
+                else
+                {
+                    if (!root->right)
+                    {
+                        root->right = new TreeNode(val);
+                        return temp;
+                    }
+                    root = root->right;
+                }
             }
+            return temp;
         }
-    TreeNode* insertIntoBST(TreeNode *root, int val)
-    {
-        if (!root)
-        {
-            root = new TreeNode(val);
-            return root;
-        }
-        insert(root, val);
-        return root;
-    }
 };
