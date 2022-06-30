aae0aa9a55a98e499f2f1229e2a4ef3ca59be08b
Varun Jain <varunj4700@gmail.com>
2022-06-30 12:49:33 +0530

    Added solution - LeetHub

---
+++ Police and Thieves - GFG/police-and-thieves.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int catchThieves(char arr[], int n, int k) 
+    {
+        vector<int>police,thief;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]=='T')
+                thief.push_back(i);
+            else 
+                police.push_back(i);
+        }
+        int i=0,j=0,ans=0;
+        while(i<police.size()&&j<thief.size())
+        {
+            if(abs(police[i]-thief[j])<=k)
+            {
+                i++;
+                j++;
+                ans++;
+            }
+            else if(police[i]>thief[j])
+                j++;
+            else 
+                i++;
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n, k;
+		cin>>n >> k;
+		char arr[n];
+		for (int i = 0; i < n; ++i)
+			cin>>arr[i];
+		Solution obj;
+		cout<<obj.catchThieves(arr, n, k) << endl;
+	}
+	return 0; 
+} 
+
+  // } Driver Code Ends
