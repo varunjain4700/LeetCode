063ef36adca2e5ce15135ef1370b760fe4986afd
Varun Jain <varunj4700@gmail.com>
2022-07-03 17:11:08 +0530

    Time: 41 ms (39.43%), Space: 13.7 MB (63.41%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,34 +1,35 @@
 class Solution
 {
     public:
-        void dfs(int src, vector<vector < int>> &adj, vector< bool > &vis)
+        void dfs(int src, vector<vector < int>> &edges, vector< bool > &vis)
         {
             vis[src] = 1;
-            for (auto child: adj[src])
+            for (int i = 0; i < edges[0].size(); i++)
             {
-                if (!vis[child])
-                    dfs(child, adj, vis);
+                if (edges[src][i] == 1 && !vis[i])
+                    dfs(i, edges, vis);
             }
         }
     int findCircleNum(vector<vector < int>> &edges)
     {
         int n = edges.size();
-        vector<vector < int>> adj(n + 1);	//adjacency list
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (j != i && edges[i][j] == 1)
-                    adj[i + 1].push_back(j + 1);
-            }
-        }
+        //using adjacency list or just use the graph given in the question
+       	// vector<vector < int>> adj(n + 1);	
+       	// for (int i = 0; i < n; i++)
+       	// {
+       	//     for (int j = 0; j < n; j++)
+       	//     {
+       	//         if (j != i && edges[i][j] == 1)
+       	//             adj[i + 1].push_back(j + 1);
+       	//     }
+       	// }
         vector<bool> vis(n + 1, 0);	//keep track of which nodes have been visited
         int cnt = 0;	//no. of connected components
-        for (int i = 1; i <= n; i++)
+        for (int i = 0; i < n; i++)
         {
             if (!vis[i])
             {
-                dfs(i, adj, vis);
+                dfs(i, edges, vis);
                 cnt++;
             }
         }
