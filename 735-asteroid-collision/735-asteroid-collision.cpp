4a2f90d73d99053ef7c3ad1cc7e10dbb87d23368
Varun Jain <varunj4700@gmail.com>
2022-07-08 19:54:13 +0530

    Time: 29 ms (23.12%), Space: 17.9 MB (40.96%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -1,33 +1,37 @@
 class Solution
 {
     public:
-        vector<int> asteroidCollision(vector<int> &nums)
+       	//collision will only take place when asteroid moving in negative/left direction is present to the right of asteroid moving in positive/right direction
+        vector<int> asteroidCollision(vector<int> &asteroids)
         {
-            vector<int> res;
-            int n = nums.size();
+            int n = asteroids.size();
             stack<int> st;
             for (int i = 0; i < n; i++)
             {
-                if (!st.empty() && st.top() > 0 && nums[i] < 0)
+                if (asteroids[i] < 0)
                 {
-                    while (!st.empty() && st.top() < abs(nums[i]) && st.top() > 0)
+                    while (!st.empty() && st.top() > 0 && st.top() < -asteroids[i])	//remove all asteroids from the left that are smaller than the current one
+                        st.pop();
+                    if (!st.empty() && st.top() > 0)
                     {
-                        st.pop();
+                        if (st.top() == -asteroids[i])	//both asteroids are of same size-->remove both(current and previous one)
+                        {
+                            st.pop();
+                            continue;
+                        }
+                        if (st.top() > -asteroids[i])	//current asteroid is smaller in size,so just don't include this in stack
+                            continue;
                     }
-                    if (!st.empty() && st.top() == abs(nums[i]))
-                        st.pop();
-                    else if (st.empty() || (!st.empty() && st.top() < 0) || (!st.empty() && st.top() > 0 && st.top() < abs(nums[i])))
-                        st.push(nums[i]);
                 }
-                else
-                    st.push(nums[i]);
+                st.push(asteroids[i]);
             }
+            vector<int> ans;
             while (!st.empty())
             {
-                res.push_back(st.top());
+                ans.push_back(st.top());
                 st.pop();
             }
-            reverse(res.begin(), res.end());
-            return res;
+            reverse(ans.begin(), ans.end());
+            return ans;
         }
 };
