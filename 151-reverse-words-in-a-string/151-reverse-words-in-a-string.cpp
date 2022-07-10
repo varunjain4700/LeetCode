faf7c86161d70d0972a145996b2fd06f2a766ad0
Varun Jain <varunj4700@gmail.com>
2022-07-10 15:35:00 +0530

    Time: 4 ms (83.90%), Space: 20.9 MB (18.81%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -3,37 +3,48 @@ class Solution
     public:
         string reverseWords(string s)
         {
-            reverse(s.begin(), s.end());
-            int n = s.size();
-            int i = 0;
-            string ans = "", temp = "";
-            bool flag = 0;
-            while (i < n)
+           	//Using stringstream (it breaks the string into a stream of words so we don't need to take care of leading or trailing spaces separately)
+            stringstream str(s);
+            string word, ans = "";
+            while (str >> word)
             {
-                while (s[i] == ' ')
-                {
-                    if (flag)
-                    {
-                        reverse(temp.begin(), temp.end());
-                        ans += temp + " ";
-                        flag = 0;
-                        temp.clear();
-                    }
-                    i++;
-                }
-                if (i < n && s[i] != ' ')
-                {
-                    temp += s[i];
-                    flag = 1;
-                    i++;
-                }
-            }
-            if (!temp.empty())
-            {
-                reverse(temp.begin(), temp.end());
-                ans += temp + " ";
+                ans = word + " " + ans;
             }
             ans.pop_back();
             return ans;
+
+           	//reverse whole string, then reverse each word separately
+           	// reverse(s.begin(), s.end());
+           	// int n = s.size();
+           	// int i = 0;
+           	// string ans = "", temp = "";
+           	// bool flag = 0;
+           	// while (i < n)
+           	// {
+           	//     while (s[i] == ' ')
+           	//     {
+           	//         if (flag)
+           	//         {
+           	//             reverse(temp.begin(), temp.end());
+           	//             ans += temp + " ";
+           	//             flag = 0;
+           	//             temp.clear();
+           	//         }
+           	//         i++;
+           	//     }
+           	//     if (i < n && s[i] != ' ')
+           	//     {
+           	//         temp += s[i];
+           	//         flag = 1;
+           	//         i++;
+           	//     }
+           	// }
+           	// if (!temp.empty())
+           	// {
+           	//     reverse(temp.begin(), temp.end());
+           	//     ans += temp + " ";
+           	// }
+           	// ans.pop_back();
+           	// return ans;
         }
 };
