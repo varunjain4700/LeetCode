f7aed63d20d93277387fabf745af5b356e91ebc6
Varun Jain <varunj4700@gmail.com>
2022-07-11 12:07:11 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (88.71%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -1,39 +1,50 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        vector<int> rightSideView(TreeNode *root)
+        void dfs(TreeNode *root, int lvl, vector<int> &res)
         {
             if (!root)
-                return {};
-            vector<int> res;
-            queue<TreeNode*> q;
-            q.push(root);
-            while (!q.empty())
-            {
-                int sz = q.size();
-                while (sz--)
-                {
-                    TreeNode *node = q.front();
-                    q.pop();
-                    if (sz == 0)
-                        res.push_back(node->val);
-                    if (node->left)
-                        q.push(node->left);
-                    if (node->right)
-                        q.push(node->right);
-                }
-            }
-            return res;
+                return;
+            if (lvl == res.size())
+                res.push_back(root->val);
+            dfs(root->right, lvl + 1, res);
+            dfs(root->left, lvl + 1, res);
         }
+    vector<int> rightSideView(TreeNode *root)
+    {
+        if (!root)
+            return {};
+        vector<int> res;
+        dfs(root, 0, res);
+        return res;
+       	// queue<TreeNode*> q;
+       	// q.push(root);
+       	// while (!q.empty())
+       	// {
+       	//     int sz = q.size();
+       	//     for (int i = 0; i < sz; i++)
+       	//     {
+       	//         TreeNode *node = q.front();
+       	//         q.pop();
+       	//         if (i == sz - 1)
+       	//             res.push_back(node->val);
+       	//         if (node->left)
+       	//             q.push(node->left);
+       	//         if (node->right)
+       	//             q.push(node->right);
+       	//     }
+       	// }
+       	// return res;
+    }
 };
