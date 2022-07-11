e58933ba470c0c525a4a3716b32c235639652431
Varun Jain <varunj4700@gmail.com>
2022-07-11 18:14:27 +0530

    Time: 79 ms (25.89%), Space: 30.1 MB (16.92%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,27 +1,43 @@
 class Solution
 {
     public:
-        int solve(int i, int j, string &s1, string &s2, vector<vector < int>> &dp)
+       	// int solve(int i, int j, string &s1, string &s2, vector<vector < int>> &dp)
+       	// {
+       	//     if (i < 0 && j >= 0)
+       	//         return 0;
+       	//     if (j < 0)
+       	//         return 1;
+       	//     if (dp[i][j] != -1)
+       	//         return dp[i][j];
+       	//     int ans = 0;
+       	//     if (s1[i] == s2[j])
+       	//         ans = solve(i - 1, j - 1, s1, s2,dp) + solve(i - 1, j, s1, s2,dp);
+       	//     else
+       	//     {
+       	//         ans += solve(i - 1, j, s1, s2,dp);
+       	//     }
+       	//     return dp[i][j] = ans;
+       	// }
+        int numDistinct(string s, string t)
         {
-            if (i < 0 && j >= 0)
-                return 0;
-            if (j < 0)
-                return 1;
-            if (dp[i][j] != -1)
-                return dp[i][j];
-            int ans = 0;
-            if (s1[i] == s2[j])
-                ans = solve(i - 1, j - 1, s1, s2,dp) + solve(i - 1, j, s1, s2,dp);
-            else
+            int m = s.size(), n = t.size();
+            vector<vector < double>> dp(m + 1, vector<double> (n + 1, 0));
+           	//long long gives runtime error don't know why
+           	// return solve(m - 1, n - 1, s, t, dp);
+            for (int i = 0; i <= m; i++)
+                dp[i][0] = 1;
+            for (int i = 1; i <= m; i++)
             {
-                ans += solve(i - 1, j, s1, s2,dp);
+                for (int j = 1; j <= n; j++)
+                {
+                    if (s[i - 1] == t[j - 1])
+                        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                    else
+                    {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
             }
-            return dp[i][j] = ans;
+            return (int) dp[m][n];
         }
-    int numDistinct(string s, string t)
-    {
-        int m = s.size(), n = t.size();
-        vector<vector < int>> dp(m + 1, vector<int> (n + 1, -1));
-        return solve(m - 1, n - 1, s, t, dp);
-    }
 };
