656e0d497b8b3e67a93b8d7249b520e5922015ac
Varun Jain <varunj4700@gmail.com>
2022-07-14 17:07:25 +0530

    Time: 4 ms (75.18%), Space: 7.3 MB (66.61%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,9 +1,6 @@
 class Solution
 {
     public:
-       	//matching all the parentheses pair and popping them
-       	//at last stack will be left with indexes which are a hurdle or not letting the string become longest valid, hence just need to check such adjacent indexes and we will get the desired answer
-       	//one thing that needs to be remembered is that in this question is that we are also putting the closing bracket in the stack 
         int longestValidParentheses(string s)
         {
             int n = s.size();
@@ -11,27 +8,24 @@ class Solution
             for (int i = 0; i < n; i++)
             {
                 if (s[i] == '(')
-                {
                     st.push(i);
-                }
                 else
                 {
-                    if (!st.empty())
+                    if (st.empty())
+                        st.push(i);
+                    else
                     {
                         if (s[st.top()] == '(')
                             st.pop();
                         else
                             st.push(i);
                     }
-                    else
-                        st.push(i);
                 }
             }
-            int ans = 0, i = n;
+            int ans = 0, i = n, j = 0;
             while (!st.empty())
             {
-                int j = st.top();
-               	//cout<<i<<" "<<j<<endl;
+                j = st.top();
                 st.pop();
                 ans = max(ans, i - j - 1);
                 i = j;
