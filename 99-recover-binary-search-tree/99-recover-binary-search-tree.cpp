cb96613b4b74ed8ad3e3e08f7ce82023c961451b
Varun Jain <varunj4700@gmail.com>
2022-07-25 17:09:15 +0530

    Time: 71 ms (20.57%), Space: 57.7 MB (98.56%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -1,43 +1,43 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *int val;
- *TreeNode * left;
- *TreeNode * right;
- *TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        /*inorder traversal of bst gives nodes in sorted order so if we don't get th
-        e nodes in sorted order it means 2 nodes are swapped.now there are 2 cases that can happen- 1) the nodes that need to be swapped are adjacent to each other  
-        2)nodes are not adjacent
-        we are maintaining a prev pointer which points to the previous node of the current node and if its val is less than the current node then it becomes a potential candidate to get swapped*/
-
-        TreeNode * first;
-    TreeNode * second;
-    TreeNode * prev;
-    void inorder(TreeNode *root)
+        TreeNode *first = NULL, *second = NULL;
+    void inorder(TreeNode *root, TreeNode* &prev)
     {
         if (!root)
             return;
-        inorder(root->left);
-        if (first == NULL && root->val < prev->val)
-            first = prev;
-        if (first != NULL && root->val < prev->val)
-            second = root;
+        inorder(root->left, prev);
+       	//cout<<root->val<<" "<<prev->val<<endl;
+        if (root->val < prev->val)
+        {
+            if (first == NULL)
+            {
+                first = prev;
+                second = root;
+            }
+            else
+                second = root;
+        }
         prev = root;
-        inorder(root->right);
+        inorder(root->right, prev);
     }
     void recoverTree(TreeNode *root)
     {
         first = NULL;
         second = NULL;
-        prev = new TreeNode(INT_MIN);	//lowest node val given in constraint
-        inorder(root);
+        TreeNode *prev = new TreeNode(INT_MIN);
+        inorder(root, prev);
         swap(first->val, second->val);
     }
 };
