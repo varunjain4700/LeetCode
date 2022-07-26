276a0047dbc6a0a6727960294dcede68aa53b496
Varun Jain <varunj4700@gmail.com>
2022-07-27 02:56:57 +0530

    Time: 13 ms (7.52%), Space: 7.2 MB (20.72%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+    public:
+        int solve(int idx, string &s, map<string, int> &mp, vector<int> &dp)
+        {
+            if (idx == s.size())
+                return 1;
+            if (dp[idx] != -1)
+                return dp[idx];
+            int ans = 0;
+            string temp = "";
+            temp += s[idx];
+            if (mp.find(temp) != mp.end())
+                ans += solve(idx + 1, s, mp, dp);
+            if (idx + 1 < s.size())
+            {
+                temp += s[idx + 1];
+                if (mp.find(temp) != mp.end())
+                    ans += solve(idx + 2, s, mp, dp);
+            }
+            return dp[idx] = ans;
+        }
+    int numDecodings(string s)
+    {
+        vector<int> dp(s.size() + 1, -1);
+        map<string, int> mp;
+        for (int i = 1; i <= 26; i++)
+        {
+            string temp = to_string(i);
+            mp[temp]++;
+        }
+        return solve(0, s, mp, dp);
+    }
+};
