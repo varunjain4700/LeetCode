9866d8164dd0517cb1ceebaa281b570faa5b90a7
Varun Jain <varunj4700@gmail.com>
2022-08-13 15:54:21 +0530

    Time: 2640 ms (5.15%), Space: 404.3 MB (5.15%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+        int mod = 1e9 + 7;
+    int solve(int idx, int seats, string &nums, int n, vector<vector < int>> &dp)
+    {
+        if (idx == n && seats == 2)
+            return 1;
+        if (idx == n)
+            return 0;
+        if (dp[idx][seats] != -1)
+            return dp[idx][seats];
+        long long ans = 0;
+        if (seats == 2)
+        {
+            if (nums[idx] == 'P')
+                ans = (solve(idx + 1, seats, nums, n, dp) % mod + solve(idx + 1, 0, nums, n, dp) % mod) % mod;
+            else
+                ans += solve(idx + 1, 1, nums, n, dp);
+        }
+        else
+        {
+            if (nums[idx] == 'S')
+                ans += solve(idx + 1, seats + 1, nums, n, dp);
+            else
+                ans += solve(idx + 1, seats, nums, n, dp);
+        }
+        ans %= mod;
+        return dp[idx][seats] = ans;
+    }
+    int numberOfWays(string nums)
+    {
+        int n = nums.size();
+        vector<vector < int>> dp(n + 1, vector<int> (3, -1));
+        return solve(0, 0, nums, n, dp);
+    }
+};
