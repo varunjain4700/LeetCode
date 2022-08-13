d6bd43a9fc3a4b0667a5797dfb3092b16e87254c
Varun Jain <varunj4700@gmail.com>
2022-08-13 15:56:10 +0530

    Time: 2519 ms (5.15%), Space: 403 MB (5.15%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -2,11 +2,11 @@ class Solution
 {
     public:
         int mod = 1e9 + 7;
-    int solve(int idx, int seats, string &nums, int n, vector<vector < int>> &dp)
+    int solve(int idx, int seats, string &nums, vector<vector < int>> &dp)
     {
-        if (idx == n && seats == 2)
+        if (idx == nums.size() && seats == 2)
             return 1;
-        if (idx == n)
+        if (idx == nums.size())
             return 0;
         if (dp[idx][seats] != -1)
             return dp[idx][seats];
@@ -14,16 +14,16 @@ class Solution
         if (seats == 2)
         {
             if (nums[idx] == 'P')
-                ans = (solve(idx + 1, seats, nums, n, dp) % mod + solve(idx + 1, 0, nums, n, dp) % mod) % mod;
+                ans = (solve(idx + 1, seats, nums, dp) % mod + solve(idx + 1, 0, nums, dp) % mod) % mod;
             else
-                ans += solve(idx + 1, 1, nums, n, dp);
+                ans += solve(idx + 1, 1, nums, dp);
         }
         else
         {
             if (nums[idx] == 'S')
-                ans += solve(idx + 1, seats + 1, nums, n, dp);
+                ans += solve(idx + 1, seats + 1, nums, dp);
             else
-                ans += solve(idx + 1, seats, nums, n, dp);
+                ans += solve(idx + 1, seats, nums, dp);
         }
         ans %= mod;
         return dp[idx][seats] = ans;
@@ -32,6 +32,6 @@ class Solution
     {
         int n = nums.size();
         vector<vector < int>> dp(n + 1, vector<int> (3, -1));
-        return solve(0, 0, nums, n, dp);
+        return solve(0, 0, nums, dp);
     }
 };
