5e07691b67e710b6263f1e797ebd6bbefc99b559
Varun Jain <varunj4700@gmail.com>
2022-08-14 22:26:38 +0530

    Time: 29 ms (14.11%), Space: 21.7 MB (70.37%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,28 +1,28 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        bool solve(TreeNode *root, long minm, long maxm)
+        bool check(TreeNode *root, long long minm, long long maxm)
         {
             if (!root)
                 return true;
-           	// cout<<minm<<" "<<maxm<<" "<<root->val<<endl;
-            if (root->val <= minm || root->val >= maxm)
+            if (root->val <=minm || root->val >=maxm)
                 return false;
-            return solve(root->left, minm, min(maxm, (long) root->val)) && solve(root->right, max((long) root->val, minm), maxm);
+            return check(root->left, minm, min((long long) root->val, maxm)) && check(root->right, max(minm, (long long) root->val), maxm);
         }
     bool isValidBST(TreeNode *root)
     {
-        return solve(root, -1e15, 1e15);
+        long long minm = -1e15, maxm = 1e15;
+        return check(root, minm, maxm);
     }
 };
