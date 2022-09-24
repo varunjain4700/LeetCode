509d171cf7a41782c97a3abbfaa2244eaccf1d6c
Varun Jain <varunj4700@gmail.com>
2022-09-24 21:12:54 +0530

    Time: 15 ms (72.38%), Space: 19.7 MB (92.80%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -1,41 +1,38 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *  int val;
+ *  TreeNode * left;
+ *  TreeNode * right;
+ *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
 {
     public:
-        void solve(TreeNode *root, int sum, int &curr_sum, vector<int> &temp, vector<vector< int>> &res)
+        void solve(TreeNode *root, int &sum, int &target, vector<int> &temp, vector<vector< int>> &res)
         {
             if (!root)
                 return;
-            curr_sum += root->val;
             temp.push_back(root->val);
-            if (!root->left && !root->right)
+            sum += root->val;
+            if (sum == target && !root->left && !root->right)
             {
-                if (curr_sum == sum)
-                {
-                    res.push_back(temp);
-                }
+                res.push_back(temp);
             }
-            solve(root->left, sum, curr_sum, temp, res);
-            solve(root->right, sum, curr_sum, temp, res);
-            curr_sum-=root->val;
+            solve(root->left, sum, target, temp, res);
+            solve(root->right, sum, target, temp, res);
+            sum -= root->val;
             temp.pop_back();
         }
-    vector<vector < int>> pathSum(TreeNode *root, int sum)
+    vector<vector < int>> pathSum(TreeNode *root, int target)
     {
         vector<vector < int>> res;
         vector<int> temp;
-        int curr_sum=0;
-        solve(root, sum, curr_sum, temp, res);
+        int sum = 0;
+        solve(root, sum, target, temp, res);
         return res;
     }
 };
